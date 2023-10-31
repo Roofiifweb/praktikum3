c263d88bbb043967e59941c600a93ceb9f8ba6b5
Roofiifweb.dll <roofiifaflah@gmail.com>
2023-10-31 19:30:51 +0700

    praktikum modul3

---
+++ src/HitungLuasKubus.java
@@ -0,0 +1,23 @@
+/**
+ * @author roofiif
+ *
+ */
+public class HitungLuasKubus {
+    public static void main(String[] args) {
+
+        double sisi = 5.0;
+        /**
+         * tipe data yang digunakan
+         */
+        double luasPermukaan = 6 * sisi * sisi;
+        /**
+         * rumus untuk mencari luas permukaan kubus
+         */
+
+        System.out.println("Luas permukaan kubus dengan sisi " + sisi + " adalah " + luasPermukaan);
+        /**
+         * sout berfungsi untuk menampilkan hasil / menampilkan output
+         */
+    }
+
+}
